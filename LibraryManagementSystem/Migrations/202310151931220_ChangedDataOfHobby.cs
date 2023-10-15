be643feb51a299bca0e748ab7b719f2560eb22fe
hash-amit <hash.amitkumar@gmail.com>
2023-10-16 02:27:43 +0530

    Added more functionalities

---
+++ LibraryManagementSystem/Migrations/202310151931220_ChangedDataOfHobby.cs
@@ -0,0 +1,18 @@
+﻿namespace LibraryManagementSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangedDataOfHobby : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.TblUsers", "Hobby", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.TblUsers", "Hobby", c => c.Int(nullable: false));
+        }
+    }
+}
---
+++ LibraryManagementSystem/Models/TblHobby1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryManagementSystem.Models
+{
+    public class TblHobby1
+    {
+        public int HobbyId { get; set; }
+        public string HobbyName { get; set; }
+        public bool IsHobby { get; set; }
+    }
+}
